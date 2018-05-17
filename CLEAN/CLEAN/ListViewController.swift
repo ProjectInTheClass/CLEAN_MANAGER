7bc66cd3d283dff88e9b54b583caa49f9608aa87
eunji <smej201@naver.com>
2018-05-17 14:55:52 +0900

    mainLayout
    #메인 화면 상단 탭바 안보이게 하였음.
    #메인 화면에서 청소할 공간 또는 물건의 목록으로 화면 전환하게 하였음.

---
+++ CLEAN/CLEAN/ListViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ListViewController.swift
+//  CLEAN
+//
+//  Created by eunji on 2018. 5. 16..
+//  Copyright © 2018년 김장현. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var num = 12
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationController?.isNavigationBarHidden = false
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return num
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "idiot", for: indexPath)
+        // cell.image =
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        num = indexPath.row
+        //collectionView.reloadData()
+        
+        if let vc = self.storyboard?.instantiateViewController(withIdentifier: "list") as? ListViewController     {
+            vc.num = indexPath.row
+            self.navigationController?.pushViewController(vc, animated: false);
+        }
+    
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
