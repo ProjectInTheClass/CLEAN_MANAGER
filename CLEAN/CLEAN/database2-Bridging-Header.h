419efddd7181dd8af28f24217ea4dcb7162ecdb3
김장현 <KimJangHyeon@gimjanghyeon-ui-MacBook-Pro.local>
2018-05-28 12:29:45 +0900

    a

---
+++ CLEAN/CLEAN/database2-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  database2-Bridging-Header.h
+//  CLEAN
+//
+//  Created by 김장현 on 2018. 5. 28..
+//  Copyright © 2018년 김장현. All rights reserved.
+//
+
+#ifndef database2_Bridging_Header_h
+#define database2_Bridging_Header_h
+
+
+#endif /* database2_Bridging_Header_h */
+
+#import "FMDB.h"
