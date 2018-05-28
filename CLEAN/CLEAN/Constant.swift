45f3639f802173ddb6287fc0bba0ec1f883da045
김장현 <KimJangHyeon@gimjanghyeon-ui-MacBook-Pro.local>
2018-05-28 16:25:37 +0900

    fmdb install

---
+++ CLEAN/CLEAN/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  CLEAN
+//
+//  Created by 김장현 on 2018. 5. 28..
+//  Copyright © 2018년 김장현. All rights reserved.
+//
+
+import Foundation
+
+struct Constants{
+    struct Database{
+        static let dbPath = NSString()
+    }
+}
---
+++ CLEAN/CLEAN/SpaceTable.swift
@@ -0,0 +1,12 @@
+//
+//  spaceTable.swift
+//  CLEAN
+//
+//  Created by 김장현 on 2018. 5. 28..
+//  Copyright © 2018년 김장현. All rights reserved.
+//
+
+import Foundation
+
+
+
