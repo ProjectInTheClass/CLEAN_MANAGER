f391553d34d67e362e0b8e5101945274b4f0d24d
KimJangHyeon <rlawkdgus75@gmail.com>
2018-06-04 11:50:46 +0900

    move static data to constants

--- CLEAN/CLEAN/Constant.swift
+++ CLEAN/CLEAN/Constant.swift
@@ -7,9 +7,27 @@
 //
 
 import Foundation
+import UIKit
 
-struct Constants{
+class Constants: NSObject{
+    
     struct Database{
         static let dbPath = NSString()
+    }
+    struct DetailSet {
+        static let alarm_data = ["소리", "진동", "무음"]
+        static let month = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"]
+        static let day = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"]
+        static let memo_place_holder = "memo..."
+        static let date_format = "MMMM - dd - YYYY"
+        static let alarm_picker_num = 1
+        static let cycle_picker_num = 2
+    }
+    struct TextShow{
+        static let space_holder_color : UIColor = UIColor.lightGray
+        static let text_color : UIColor = UIColor.black
+    }
+    struct Button{
+        static let done = "done"
     }
 }
